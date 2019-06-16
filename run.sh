992bfc258e92ebffea3cee5205c89c69dbf83be6
TinyAllen <Allen.Wang.123@outlook.com>
2019-06-16 20:45:32 +0800

    updte

--- run.sh
+++ run.sh
@@ -8,14 +8,14 @@ absolute_path="/etc/nginx/nginx.conf"
 mv ${absolute_path} ${absolute_path}.old
 cat ${absolute_path}.old | sed s#BASE_URL#${BASE_URL}#g    >  ${absolute_path}
 
-filename=`ls /usr/share/nginx/html/js/ | grep "index.*.js$"`
+filename=`ls /usr/share/nginx/html/js/ | grep "app.*.js$"`
 absolute_path2='/usr/share/nginx/html/js/'${filename}
 
 mv ${absolute_path2} ${absolute_path2}.old
 cat ${absolute_path2}.old |
   sed s#BASE_URL#${BASE_URL}#g             >  ${absolute_path2}
 
-filename=`ls /usr/share/nginx/html/js/ | grep "common.*.js$"`
+filename=`ls /usr/share/nginx/html/js/ | grep "chunk-vendors.*.js$"`
 absolute_path3='/usr/share/nginx/html/js/'${filename}
 
 mv ${absolute_path3} ${absolute_path3}.old
